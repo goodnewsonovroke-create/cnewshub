ec51def730d8df879e0c32c4bc3a634152b6ebad
Goodnews Onovroke <goodnewsonovroke@gmail.com>
2026-06-02 05:20:30 -0600

    Fix: add missing comma before authPlugins

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  allowPublicKeyRetrieval: true,
+  allowPublicKeyRetrieval: true, // <--- CHECK THIS LINE! Make sure this comma is here
   authPlugins: {
     mysql_clear_password: () => Buffer.from(process.env.DB_PASSWORD + '\0')
   }
